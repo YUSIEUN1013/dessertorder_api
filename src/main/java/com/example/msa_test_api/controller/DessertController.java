366f01a4948557b4a6a2f328a237568865f0a227
YUSIEUN1013 <db4067ww@gmail.com>
2023-11-17 15:33:25 +0900

    api test

--- src/main/java/com/example/msa_test_api/controller/DessertController.java
+++ src/main/java/com/example/msa_test_api/controller/DessertController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://101.55.69.114:31939")
 @RestController
 public class DessertController {
 
